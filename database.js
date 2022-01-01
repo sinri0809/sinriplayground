c4eff12dd52465b38a01987e7b5cb8e8dff2652e
sinri <49238705+sinri0809@users.noreply.github.com>
2022-01-01 20:43:15 +0900

    1.2 기능 구현
    댓글 창 기능 추가(수정, 삭제)
    로그인 기능 수정 및 추가

---
+++ database.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+
+import firebase from 'firebase/compat/app';
+import { getAuth } from "firebase/auth" 
+import { getFirestore, collection } from "@firebase/firestore"
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
+const bucketUrl = "gs://sinriplayground.appspot.com";
+
+const sinri = {
+  id: process.env.REACT_APP_OWNER
+}
+
+const app = firebase.initializeApp(firebaseConfig);
+const authService = getAuth(app);
+const connectDB = getFirestore(app);
+const fbStorage = getStorage(app, bucketUrl);
+const commentRef = collection(connectDB, "commentHome");
+
+export { authService, sinri, connectDB, fbStorage, commentRef };
